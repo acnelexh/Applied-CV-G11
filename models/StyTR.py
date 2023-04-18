7c05ce3eb2e1f063b7a9b1074e463e3d5fd8eace
劉政宇 <harryliu@64.121.254.198.naturalwireless.com>
2023-04-17 21:16:42 -0400

    init commit

--- models/StyTR.py
+++ models/StyTR.py
@@ -134,97 +134,105 @@ class MLP(nn.Module):
         for i, layer in enumerate(self.layers):
             x = F.relu(layer(x)) if i < self.num_layers - 1 else layer(x)
         return x
-class StyTrans(nn.Module):
-    """ This is the style transform transformer module """
+# class StyTrans(nn.Module):
+#     """ This is the style transform transformer module """
     
-    def __init__(self,encoder,decoder,PatchEmbed, transformer,args):
+#     def __init__(self,encoder,decoder,PatchEmbed, transformer,args):
 
-        super().__init__()
-        enc_layers = list(encoder.children())
-        self.enc_1 = nn.Sequential(*enc_layers[:4])  # input -> relu1_1
-        self.enc_2 = nn.Sequential(*enc_layers[4:11])  # relu1_1 -> relu2_1
-        self.enc_3 = nn.Sequential(*enc_layers[11:18])  # relu2_1 -> relu3_1
-        self.enc_4 = nn.Sequential(*enc_layers[18:31])  # relu3_1 -> relu4_1
-        self.enc_5 = nn.Sequential(*enc_layers[31:44])  # relu4_1 -> relu5_1
+#         super().__init__()
+#         enc_layers = list(encoder.children())
+#         self.enc_1 = nn.Sequential(*enc_layers[:4])  # input -> relu1_1
+#         self.enc_2 = nn.Sequential(*enc_layers[4:11])  # relu1_1 -> relu2_1
+#         self.enc_3 = nn.Sequential(*enc_layers[11:18])  # relu2_1 -> relu3_1
+#         self.enc_4 = nn.Sequential(*enc_layers[18:31])  # relu3_1 -> relu4_1
+#         self.enc_5 = nn.Sequential(*enc_layers[31:44])  # relu4_1 -> relu5_1
         
-        for name in ['enc_1', 'enc_2', 'enc_3', 'enc_4', 'enc_5']:
-            for param in getattr(self, name).parameters():
-                param.requires_grad = False
+#         for name in ['enc_1', 'enc_2', 'enc_3', 'enc_4', 'enc_5']:
+#             for param in getattr(self, name).parameters():
+#                 param.requires_grad = False
 
-        self.mse_loss = nn.MSELoss()
-        self.transformer = transformer
-        hidden_dim = transformer.d_model       
-        self.decode = decoder
-        self.embedding = PatchEmbed
+#         self.mse_loss = nn.MSELoss()
+#         self.transformer = transformer
+#         hidden_dim = transformer.d_model       
+#         self.decode = decoder
+#         self.embedding = PatchEmbed
 
-    def encode_with_intermediate(self, input):
-        results = [input]
-        for i in range(5):
-            func = getattr(self, 'enc_{:d}'.format(i + 1))
-            results.append(func(results[-1]))
-        return results[1:]
+#     def encode_with_intermediate(self, input):
+#         results = [input]
+#         for i in range(5):
+#             func = getattr(self, 'enc_{:d}'.format(i + 1))
+#             results.append(func(results[-1]))
+#         return results[1:]
 
-    def calc_content_loss(self, input, target):
-      assert (input.size() == target.size())
-      assert (target.requires_grad is False)
-      return self.mse_loss(input, target)
+#     def calc_content_loss(self, input, target):
+#       assert (input.size() == target.size())
+#       assert (target.requires_grad is False)
+#       return self.mse_loss(input, target)
 
-    def calc_style_loss(self, input, target):
-        assert (input.size() == target.size())
-        assert (target.requires_grad is False)
-        input_mean, input_std = calc_mean_std(input)
-        target_mean, target_std = calc_mean_std(target)
-        return self.mse_loss(input_mean, target_mean) + \
-               self.mse_loss(input_std, target_std)
-    def forward(self, samples_c: NestedTensor,samples_s: NestedTensor):
-        """ The forward expects a NestedTensor, which consists of:
-               - samples.tensor: batched images, of shape [batch_size x 3 x H x W]
-               - samples.mask: a binary mask of shape [batch_size x H x W], containing 1 on padded pixels
+#     def calc_style_loss(self, input, target):
+#         assert (input.size() == target.size())
+#         assert (target.requires_grad is False)
+#         input_mean, input_std = calc_mean_std(input)
+#         target_mean, target_std = calc_mean_std(target)
+#         return self.mse_loss(input_mean, target_mean) + \
+#                self.mse_loss(input_std, target_std)
+#     def forward(self, samples_c: NestedTensor,samples_s: NestedTensor):
+#         """ The forward expects a NestedTensor, which consists of:
+#                - samples.tensor: batched images, of shape [batch_size x 3 x H x W]
+#                - samples.mask: a binary mask of shape [batch_size x H x W], containing 1 on padded pixels
 
-        """
-        content_input = samples_c
-        style_input = samples_s
-        if isinstance(samples_c, (list, torch.Tensor)):
-            samples_c = nested_tensor_from_tensor_list(samples_c)   # support different-sized images padding is used for mask [tensor, mask] 
-        if isinstance(samples_s, (list, torch.Tensor)):
-            samples_s = nested_tensor_from_tensor_list(samples_s) 
+#         """
+#         content_input = samples_c
+#         style_input = samples_s
+#         if isinstance(samples_c, (list, torch.Tensor)):
+#             samples_c = nested_tensor_from_tensor_list(samples_c)   # support different-sized images padding is used for mask [tensor, mask] 
+#         if isinstance(samples_s, (list, torch.Tensor)):
+#             samples_s = nested_tensor_from_tensor_list(samples_s) 
         
-        # ### features used to calcate loss 
-        content_feats = self.encode_with_intermediate(samples_c.tensors)
-        style_feats = self.encode_with_intermediate(samples_s.tensors)
+#         # ### features used to calcate loss 
+#         content_feats = self.encode_with_intermediate(samples_c.tensors)
+#         style_feats = self.encode_with_intermediate(samples_s.tensors)
 
-        ### Linear projection
-        style = self.embedding(samples_s.tensors)
-        content = self.embedding(samples_c.tensors)
+#         ### Linear projection
+#         style = self.embedding(samples_s.tensors)
+#         content = self.embedding(samples_c.tensors)
         
-        # postional embedding is calculated in transformer.py
-        pos_s = None
-        pos_c = None
+#         # postional embedding is calculated in transformer.py
+#         pos_s = None
+#         pos_c = None
 
-        mask = None
-        hs = self.transformer(style, mask , content, pos_c, pos_s)   
-        Ics = self.decode(hs)
+#         mask = None
+#         hs = self.transformer(style, mask , content, pos_c, pos_s)   
+#         Ics = self.decode(hs)
 
-        Ics_feats = self.encode_with_intermediate(Ics)
-        loss_c = self.calc_content_loss(normal(Ics_feats[-1]), normal(content_feats[-1]))+self.calc_content_loss(normal(Ics_feats[-2]), normal(content_feats[-2]))
-        # Style loss
-        loss_s = self.calc_style_loss(Ics_feats[0], style_feats[0])
-        for i in range(1, 5):
-            loss_s += self.calc_style_loss(Ics_feats[i], style_feats[i])
+#         Ics_feats = self.encode_with_intermediate(Ics)
+#         loss_c = self.calc_content_loss(normal(Ics_feats[-1]), normal(content_feats[-1]))+self.calc_content_loss(normal(Ics_feats[-2]), normal(content_feats[-2]))
+#         # Style loss
+#         loss_s = self.calc_style_loss(Ics_feats[0], style_feats[0])
+#         for i in range(1, 5):
+#             loss_s += self.calc_style_loss(Ics_feats[i], style_feats[i])
             
         
-        Icc = self.decode(self.transformer(content, mask , content, pos_c, pos_c))
-        Iss = self.decode(self.transformer(style, mask , style, pos_s, pos_s))    
+#         Icc = self.decode(self.transformer(content, mask , content, pos_c, pos_c))
+#         Iss = self.decode(self.transformer(style, mask , style, pos_s, pos_s))    
 
-        #Identity losses lambda 1    
-        loss_lambda1 = self.calc_content_loss(Icc,content_input)+self.calc_content_loss(Iss,style_input)
+#         #Identity losses lambda 1    
+#         loss_lambda1 = self.calc_content_loss(Icc,content_input)+self.calc_content_loss(Iss,style_input)
         
-        #Identity losses lambda 2
-        Icc_feats=self.encode_with_intermediate(Icc)
-        Iss_feats=self.encode_with_intermediate(Iss)
-        loss_lambda2 = self.calc_content_loss(Icc_feats[0], content_feats[0])+self.calc_content_loss(Iss_feats[0], style_feats[0])
-        for i in range(1, 5):
-            loss_lambda2 += self.calc_content_loss(Icc_feats[i], content_feats[i])+self.calc_content_loss(Iss_feats[i], style_feats[i])
-        # Please select and comment out one of the following two sentences
-        return Ics,  loss_c, loss_s, loss_lambda1, loss_lambda2   #train
-        # return Ics    #test 
+#         #Identity losses lambda 2
+#         Icc_feats=self.encode_with_intermediate(Icc)
+#         Iss_feats=self.encode_with_intermediate(Iss)
+#         loss_lambda2 = self.calc_content_loss(Icc_feats[0], content_feats[0])+self.calc_content_loss(Iss_feats[0], style_feats[0])
+#         for i in range(1, 5):
+#             loss_lambda2 += self.calc_content_loss(Icc_feats[i], content_feats[i])+self.calc_content_loss(Iss_feats[i], style_feats[i])
+#         # Please select and comment out one of the following two sentences
+#         return Ics,  loss_c, loss_s, loss_lambda1, loss_lambda2   #train
+#         # return Ics    #test 
+
+# returns a batch of images
+class StyTrans(nn.Module):
+    def __init__(self, vit_pretrain_path):
+         super().__init__()
+    
+    def forward(self):
+        pass
--- train.py
+++ train.py
@@ -6,14 +6,18 @@ import torch.utils.data as data
 from PIL import Image
 from PIL import ImageFile
 from tensorboardX import SummaryWriter
-from torchvision import transforms
+from torchvision import transforms, models
 from tqdm import tqdm
 from pathlib import Path
 import models.transformer as transformer
 import models.StyTR  as StyTR 
 from sampler import InfiniteSamplerWrapper
 from torchvision.utils import save_image
+from dataset import FlatFolderDataset, ImageTokenDataset, RandomTextDataset
 
+from transformers import AutoTokenizer, CLIPTextModel, CLIPImageProcessor, CLIPVisionModel
+from template import imagenet_templates
+from util.clip_utils import get_features
 
 def train_transform():
     transform_list = [
@@ -22,6 +26,30 @@ def train_transform():
         transforms.ToTensor()
     ]
     return transforms.Compose(transform_list)
+
+def img_normalize(image):
+    mean=torch.tensor([0.485, 0.456, 0.406]).to(device)
+    std=torch.tensor([0.229, 0.224, 0.225]).to(device)
+    mean = mean.view(1,-1,1,1)
+    std = std.view(1,-1,1,1)
+
+    image = (image-mean)/std
+    return image
+
+def encode_img(image, image_processor, image_encoder):
+    image = image_processor(image)
+    image['pixel_values'] = torch.tensor(image['pixel_values']) #?
+    image = image_encoder(**image)
+    return image.last_hidden_state.squeeze(0)
+
+def get_image_prior_losses(inputs_jit):
+    diff1 = inputs_jit[:, :, :, :-1] - inputs_jit[:, :, :, 1:]
+    diff2 = inputs_jit[:, :, :-1, :] - inputs_jit[:, :, 1:, :]
+    diff3 = inputs_jit[:, :, 1:, :-1] - inputs_jit[:, :, :-1, 1:]
+    diff4 = inputs_jit[:, :, :-1, :-1] - inputs_jit[:, :, 1:, 1:]
+
+    loss_var_l2 = torch.norm(diff1) + torch.norm(diff2) + torch.norm(diff3) + torch.norm(diff4)
+    return loss_var_l2
 
 
 def adjust_learning_rate(optimizer, iteration_count):
@@ -40,13 +68,13 @@ def warmup_learning_rate(optimizer, iteration_count):
 
 parser = argparse.ArgumentParser()
 # Basic options
-parser.add_argument('--content_dir', default='./datasets/train2014', type=str,   
+parser.add_argument('--content_dir', type=str,   
                     help='Directory path to a batch of content images')
-parser.add_argument('--style_dir', default='./datasets/Images', type=str,  #wikiart dataset crawled from https://www.wikiart.org/
-                    help='Directory path to a batch of style images')
+parser.add_argument('--style_texts', type=list[str],
+                    help='List of style texts')
 parser.add_argument('--vgg', type=str, default='./experiments/vgg_normalised.pth')  #run the train.py, please download the pretrained vgg checkpoint
 
-# training options
+# training options TODO: modify options
 parser.add_argument('--save_dir', default='./experiments',
                     help='Directory to save the model')
 parser.add_argument('--log_dir', default='./logs',
@@ -55,14 +83,13 @@ parser.add_argument('--lr', type=float, default=5e-4)
 parser.add_argument('--lr_decay', type=float, default=1e-5)
 parser.add_argument('--max_iter', type=int, default=160000)
 parser.add_argument('--batch_size', type=int, default=8)
-parser.add_argument('--style_weight', type=float, default=10.0)
-parser.add_argument('--content_weight', type=float, default=7.0)
+parser.add_argument('--lambda_tv', type=float, default=2e-3)
+parser.add_argument('--lambda_patch', type=float, default=9000)
+parser.add_argument('--lambda_dir', type=float, default=500)
+parser.add_argument('--lambda_c', type=float, default=150)
 parser.add_argument('--n_threads', type=int, default=16)
 parser.add_argument('--save_model_interval', type=int, default=10000)
-parser.add_argument('--position_embedding', default='sine', type=str, choices=('sine', 'learned'),
-                        help="Type of positional embedding to use on top of the image features")
-parser.add_argument('--hidden_dim', default=512, type=int,
-                        help="Size of the embeddings (dimension of the transformer)")
+parser.add_argument('--thresh', type=float, default=0.7)
 args = parser.parse_args()
 
 USE_CUDA = torch.cuda.is_available()
@@ -75,49 +102,70 @@ if not os.path.exists(args.log_dir):
     os.mkdir(args.log_dir)
 writer = SummaryWriter(log_dir=args.log_dir)
 
-vgg = StyTR.vgg
-vgg.load_state_dict(torch.load(args.vgg))
-vgg = nn.Sequential(*list(vgg.children())[:44])
+vgg = models.vgg19(pretrained=True).features
+vgg.to(device)
+for parameter in vgg.parameters():
+    parameter.requires_grad_(False)
 
-decoder = StyTR.decoder
-embedding = StyTR.PatchEmbed()
 
-Trans = transformer.Transformer()
-with torch.no_grad():
-    network = StyTR.StyTrans(vgg,decoder,embedding, Trans,args)
-network.train()
-
+network = StyTR.StyTrans() # TODO: add ViT pretrain path if needed
 network.to(device)
-network = nn.DataParallel(network, device_ids=[0,1])
-content_tf = train_transform()
-style_tf = train_transform()
+#network = nn.DataParallel(network, device_ids=[0,1]) # probably don't need it 
+# content_tf = train_transform()
+# style_tf = train_transform()
 
+content_dataset = ImageTokenDataset(args.content_dir)
+style_dataset = RandomTextDataset(args.style_texts) #TODO: try multiple styles?
 
+# probably shouldn't do this if not necessary, wasting mem
+source = ["a photo"] * len(args.style_texts)
+source_dataset = RandomTextDataset(source)
 
-content_dataset = FlatFolderDataset(args.content_dir, content_tf)
-style_dataset = FlatFolderDataset(args.style_dir, style_tf)
-
+# returns image embedding (source_features)
 content_iter = iter(data.DataLoader(
     content_dataset, batch_size=args.batch_size,
     sampler=InfiniteSamplerWrapper(content_dataset),
     num_workers=args.n_threads))
+
+# TODO: check, returns text_features (text embedding), normalize?
 style_iter = iter(data.DataLoader(
     style_dataset, batch_size=args.batch_size,
     sampler=InfiniteSamplerWrapper(style_dataset),
     num_workers=args.n_threads))
+
+source_iter = iter(data.DataLoader(
+    source_dataset, batch_size=args.batch_size,
+    sampler=InfiniteSamplerWrapper(source_dataset),
+    num_workers=args.n_threads))
  
 
-optimizer = torch.optim.Adam([ 
-                              {'params': network.module.transformer.parameters()},
-                              {'params': network.module.decode.parameters()},
-                              {'params': network.module.embedding.parameters()},        
+optimizer = torch.optim.Adam([ # TODO: double check with Alex
+                              {'params': network.module.encoder.parameters()}, # or network.parameters()?
+                              #{'params': network.module.decode.parameters()},
+                              #{'params': network.module.embedding.parameters()},        
                               ], lr=args.lr)
 
+# TODO: discuss with team: use clip_styler sched policy or don't change
 
 if not os.path.exists(args.save_dir+"/test"):
     os.makedirs(args.save_dir+"/test")
 
+content_loss_epoch = []
+style_loss_epoch = []
+total_loss_epoch = []
 
+cropper = transforms.Compose([
+    transforms.RandomCrop(args.crop_size)
+])
+augment = transforms.Compose([
+    transforms.RandomPerspective(fill=0, p=1,distortion_scale=0.5),
+    transforms.Resize(224)
+])
+
+num_crops = args.num_crops # TODO: add args
+image_processor = CLIPImageProcessor()
+image_encoder = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
+source_features = None #TODO: raw images, not embedding
 
 for i in tqdm(range(args.max_iter)):
 
@@ -127,59 +175,96 @@ for i in tqdm(range(args.max_iter)):
         adjust_learning_rate(optimizer, iteration_count=i)
 
     # print('learning_rate: %s' % str(optimizer.param_groups[0]['lr']))
-    content_images = next(content_iter).to(device)
-    style_images = next(style_iter).to(device)  
-    out, loss_c, loss_s,l_identity1, l_identity2 = network(content_images, style_images)
+    content_images = next(content_iter).to(device) # TODO: should prob return both raw imgs and embeddings
+    style_texts = next(style_iter).to(device)
+    source_texts = next(source_iter).to(device)
 
-    if i % 100 == 0:
-        output_name = '{:s}/test/{:s}{:s}'.format(
-                        args.save_dir, str(i),".jpg"
-                    )
-        out = torch.cat((content_images,out),0)
-        out = torch.cat((style_images,out),0)
-        save_image(out, output_name)
+    targets = network(content_images)
+    targets.requires_grad_(True)
 
-        
-    loss_c = args.content_weight * loss_c
-    loss_s = args.style_weight * loss_s
-    loss = loss_c + loss_s + (l_identity1 * 70) + (l_identity2 * 1) 
-  
-    print(loss.sum().cpu().detach().numpy(),"-content:",loss_c.sum().cpu().detach().numpy(),"-style:",loss_s.sum().cpu().detach().numpy()
-              ,"-l1:",l_identity1.sum().cpu().detach().numpy(),"-l2:",l_identity2.sum().cpu().detach().numpy()
-              )
-       
+    content_features = [] # list of dict
+    for i in content_images:
+        content_features.append(get_features(img_normalize(i), vgg)) # TODO: IMPORTANT, get_features needs raw images, not embeddings
+
+    target_features = [] # dict of hidden state from vgg of images
+    for i in targets:
+        target_features.append(get_features(img_normalize(i), vgg)) 
+
+    content_loss = 0
+    content_loss += torch.mean((target_features[i]['conv4_2'] - content_features[i]['conv4_2']) ** 2)
+    content_loss += torch.mean((target_features[i]['conv5_2'] - content_features[i]['conv5_2']) ** 2)
+
+    patch_loss = 0
+    img_aug = []
+    for target in targets:
+        for n in range(num_crops):
+            target_crop = cropper(target)
+            target_crop = augment(target_crop)
+            img_aug.append(target_crop)
+    
+    # patch loss  
+    img_aug_features = [encode_img(img_aug[i], image_processor, image_encoder) for i in img_aug]
+    img_aug_features = torch.tensor(img_aug_features)
+    img_aug_features /= (img_aug_features.clone().norm(dim=-1, keepdim=True)) # TODO: check dim
+    img_direction = img_aug_features - source_features # might need for loop when implement later, should provide indexing for source_features.
+
+    text_direction = (style_texts - source_texts).repeat(img_aug_features.size(0),1) # TODO: check dim
+    text_direction /= text_direction.norm(dim=-1, keepdim=True)
+    tmp_loss = (1- torch.consine_similarity(img_direction, text_direction, dim=1))
+    tmp_loss[tmp_loss < args.thresh] = 0 # TODO: add args
+    patch_loss += tmp_loss.mean()
+
+    # global loss
+    glob_features = [encode_img(i, image_processor, image_encoder) for i in targets] # TODO: should provide index to get embeddings.
+    
+    glob_direction = (glob_features - source_features)
+    glob_direction /= glob_direction.clone().norm(dim=-1, keepdim=True)
+
+    glob_loss = (1 - torch.cosine_similarity(glob_direction, text_direction, dim=1)).mean()
+
+    var_loss = get_image_prior_losses(targets) # total variation loss
+
+    total_loss = args.lambda_patch * patch_loss + args.content_weight * content_loss + args.lambda_tv * var_loss + args.lambda_dir * glob_loss
+    total_loss_epoch.append(total_loss)
     optimizer.zero_grad()
-    loss.sum().backward()
+    total_loss.backward()
     optimizer.step()
+    ####
 
-    writer.add_scalar('loss_content', loss_c.sum().item(), i + 1)
-    writer.add_scalar('loss_style', loss_s.sum().item(), i + 1)
-    writer.add_scalar('loss_identity1', l_identity1.sum().item(), i + 1)
-    writer.add_scalar('loss_identity2', l_identity2.sum().item(), i + 1)
-    writer.add_scalar('total_loss', loss.sum().item(), i + 1)
+    if i % 50 == 0:
+        # output_name = '{:s}/test/{:s}{:s}'.format(
+        #                 args.save_dir, str(i),".jpg"
+        #             )
+        # out = torch.cat((content_images,out),0)
+        # out = torch.cat((style_images,out),0)
+        # save_image(out, output_name)
+        print("After %d criterions:" % i)
+        print('Total loss: ', total_loss.item())
+        print('Content loss: ', content_loss.item())
+        print('patch loss: ', patch_loss.item())
+        print('dir loss: ', glob_loss.item())
+        print('TV loss: ', var_loss.item())
+
+
+    # writer.add_scalar('loss_content', loss_c.sum().item(), i + 1)
+    # writer.add_scalar('loss_style', loss_s.sum().item(), i + 1)
+    # writer.add_scalar('loss_identity1', l_identity1.sum().item(), i + 1)
+    # writer.add_scalar('loss_identity2', l_identity2.sum().item(), i + 1)
+    # writer.add_scalar('total_loss', loss.sum().item(), i + 1)
+    writer.add_scaler('Total loss: ', total_loss.item())
+    writer.add_scalar('Content loss ', content_loss.item())
+    writer.add_scalar('patch loss: ', patch_loss.item())
+    writer.add_scalar('dir loss: ', glob_loss.item())
+    writer.add_scalar('TV loss: ', var_loss.item())
 
     if (i + 1) % args.save_model_interval == 0 or (i + 1) == args.max_iter:
-        state_dict = network.module.transformer.state_dict()
+        state_dict = network.module.encoder.state_dict()
         for key in state_dict.keys():
             state_dict[key] = state_dict[key].to(torch.device('cpu'))
         torch.save(state_dict,
-                   '{:s}/transformer_iter_{:d}.pth'.format(args.save_dir,
+                   '{:s}/encoder_iter_{:d}.pth'.format(args.save_dir,
                                                            i + 1))
-
-        state_dict = network.module.decode.state_dict()
-        for key in state_dict.keys():
-            state_dict[key] = state_dict[key].to(torch.device('cpu'))
-        torch.save(state_dict,
-                   '{:s}/decoder_iter_{:d}.pth'.format(args.save_dir,
-                                                           i + 1))
-        state_dict = network.module.embedding.state_dict()
-        for key in state_dict.keys():
-            state_dict[key] = state_dict[key].to(torch.device('cpu'))
-        torch.save(state_dict,
-                   '{:s}/embedding_iter_{:d}.pth'.format(args.save_dir,
-                                                           i + 1))
-
-                                                    
+                                                   
 writer.close()
 
 
