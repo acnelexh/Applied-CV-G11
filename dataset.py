fix image normalization issue + using mse loss for content loss for now

dataset.py
@@ -1,20 +1,11 @@
 import torch
 import torchvision
 import numpy as np
+import PIL
 from pathlib import Path
 import torch.utils.data as data
 from transformers import AutoTokenizer, CLIPTextModel, CLIPImageProcessor, CLIPVisionModel
 from template import imagenet_templates
-
-class VGGNormalizer():
-    def __init__(self, device='cpu', mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
-        self.mean = torch.tensor(mean).view(1,-1,1,1).to(device)
-        self.std = torch.tensor(std).view(1,-1,1,1).to(device)
-        self.transform = torchvision.transforms.Compose(
-            [torchvision.transforms.Resize(size=(224, 224))])
-    
-    def __call__(self, x) -> torch.Tensor:
-        return self.transform((x-self.mean)/self.std)
     
 class ImageTokenDataset(data.Dataset):
     '''
@@ -22,12 +13,13 @@ class ImageTokenDataset(data.Dataset):
     '''
     def __init__(self,
                 image_dir: Path,
+                image_size: int = 224,
                 device='cpu'):
         super(ImageTokenDataset, self).__init__()
         self.image_dir = image_dir
         self.image_processor = CLIPImageProcessor()
         self.device = device
-        self.vgg_transform = VGGNormalizer(device=device)
+        self.resize = torchvision.transforms.Resize(size=(image_size, image_size))
 
         self.images = [f for f in self.image_dir.glob('*')]
 
@@ -36,13 +28,15 @@ class ImageTokenDataset(data.Dataset):
         Return processed images
         One is preprocessed by CLIPImageProcessor, one is by source transform
         ''' 
-        raw_img = torchvision.io.read_image(str(self.images[index])).to(self.device)
+        raw_img = PIL.Image.open(str(self.images[index]))
+        raw_img = torchvision.transforms.functional.to_tensor(raw_img).to(self.device)
+        #raw_img = torchvision.io.read_image(str(self.images[index]), ).to(self.device)
         img = self.image_processor(raw_img)
         img = torch.tensor(np.array(img['pixel_values'])).squeeze(0).to(self.device)
         #source_img = torchvision.io.read_image(str(self.images[index]))
-        vgg_img = self.vgg_transform(raw_img).squeeze(0)
+        resize_img = self.resize(raw_img).squeeze(0)
         # for model, pathloss, vgg content loss
-        return img, vgg_img
+        return img, resize_img
 
     def __len__(self):
         return len(self.images)

loss.py
@@ -1,45 +1,53 @@
 import clip
 import torch
 from torchvision import transforms
+from torch.nn.functional import mse_loss
 from transformers import CLIPImageProcessor
 from util.clip_utils import get_features
 
-class VGGNormalizer():
-    def __init__(self, device='cpu', mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
-        self.mean = torch.tensor(mean).view(1,-1,1,1).to(device)
-        self.std = torch.tensor(std).view(1,-1,1,1).to(device)
-        self.transform = transforms.Compose(
-            [transforms.Resize(size=(224, 224))])
+class CLIPNormalizer():
+    def __init__(self, mean=[0.48145466, 0.4578275, 0.40821073], std=[0.26862954, 0.26130258, 0.27577711],device='cuda'):
+        self.transform = transforms.Compose([
+            transforms.Normalize(mean=mean, std=std)])
     
-    def __call__(self, x):
-        return self.transform((x-self.mean)/self.std)
+    def __call__(self, x) -> torch.Tensor:
+        return self.transform(x)
+
+class VGGNormalizer():
+    def __init__(self, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+        self.transform = transforms.Compose([
+            transforms.Normalize(mean=mean, std=std)])
+    
+    def __call__(self, x) -> torch.Tensor:
+        return self.transform(x)
     
 def get_content_loss(input_image, output_image, vgg, device='cuda'):
     '''
     Calculate content loss
     '''
     # noramlize targets to 0-1
-    output_image = output_image.reshape((output_image.shape[0], output_image.shape[1], -1))
-    output_image -= torch.min(output_image, dim=2, keepdim=True)[0]
-    output_image /= torch.max(output_image, dim=2, keepdim=True)[0]
-    output_image = output_image.reshape((output_image.shape[0],output_image.shape[1], 224, 224))
+    normalizer = VGGNormalizer()
+    input_image = normalizer(input_image)
+    output_image = normalizer(output_image)
 
-    content_features = [] # list of dict
+    # using mse for now
+    return mse_loss(input_image, output_image)
+    # content_features = [] # list of dict
     
-    for img in input_image:
-        content_features.append(get_features(img, vgg))
+    # for img in input_image:
+    #     content_features.append(get_features(img, vgg))
 
-    target_features = [] # dict of hidden state from vgg of images
-    VGGNORM = VGGNormalizer(device)
-    for img in output_image:
-        target_features.append(get_features(VGGNORM(img), vgg))
+    # target_features = [] # dict of hidden state from vgg of images
+    # VGGNORM = VGGNormalizer(device)
+    # for img in output_image:
+    #     target_features.append(get_features(VGGNORM(img), vgg))
     
-    content_loss = 0
-    for i in range(len(target_features)):
-        content_loss += torch.mean((target_features[i]['conv4_2'] - content_features[i]['conv4_2']) ** 2)
-        content_loss += torch.mean((target_features[i]['conv5_2'] - content_features[i]['conv5_2']) ** 2)
+    # content_loss = 0
+    # for i in range(len(target_features)):
+    #     content_loss += torch.mean((target_features[i]['conv4_2'] - content_features[i]['conv4_2']) ** 2)
+    #     content_loss += torch.mean((target_features[i]['conv5_2'] - content_features[i]['conv5_2']) ** 2)
 
-    return content_loss
+    # return content_loss
         
 def get_text_direction(source_text, style_text, model, device='cpu'):
     '''
@@ -55,14 +63,9 @@ def get_text_direction(source_text, style_text, model, device='cpu'):
     text_direction = text_direction / text_direction.norm(dim=-1, keepdim=True)
     return text_direction
     
-def encode_img(images, model, device='cpu', preprocess=True):
+def encode_img(images, model):
     '''use clip api to encode image into 512-dim vector'''
-    if preprocess:
-        preprocessor = CLIPImageProcessor(device=device) # turns out to be exactly the same as the one in clip
-        image = preprocessor(images)
-        image_features = model.encode_image(torch.tensor(image['pixel_values']).to(device))
-    else:
-        image_features = model.encode_image(images)
+    image_features = model.encode_image(images)
     return image_features
 
 def get_patches(imgs, args):
@@ -91,15 +94,20 @@ def get_img_direction(input_img, output_img, args, model, patch=False):
     '''
     Calculate image direction
     '''
+    normalizer = CLIPNormalizer()
     if patch == True:
-        output_img = get_patches(output_img, args)
-        crop_features = encode_img(output_img, model, device=args.device) # (batch_size x num_crops) x 512
+        output_image = get_patches(output_img, args)
+        # output_image = output_image.reshape((output_image.shape[0], output_image.shape[1], -1))
+        # output_image -= torch.min(output_image, dim=2, keepdim=True)[0]
+        # output_image /= torch.max(output_image, dim=2, keepdim=True)[0]
+        # output_image = output_image.reshape((output_image.shape[0],output_image.shape[1], 224, 224))
+        crop_features = encode_img(normalizer(output_image), model) # (batch_size x num_crops) x 512
         crop_features = crop_features.reshape((args.batch_size, args.num_crops, -1))
         image_features = torch.sum(crop_features, dim=1).clone()
     else:
-        image_features = encode_img(output_img, model, device=args.device) # batch_size x 512
+        image_features = encode_img(output_img, model) # batch_size x 512
         
-    source_features = encode_img(input_img, model, device=args.device, preprocess=False)
+    source_features = encode_img(input_img, model)
 
     img_direction = (image_features-source_features)
     img_direction /= img_direction.clone().norm(dim=-1, keepdim=True)